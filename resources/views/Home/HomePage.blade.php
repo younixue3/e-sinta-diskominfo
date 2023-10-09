eb8ae5b3704bba2c64284bd1a48d2193a5972997
RICKO CAESAR APRILLA TIAKA <2011102441099@umkt.ac.id>
2023-10-09 14:53:03 +0800

    Add Landing page

--- resources/views/Home/HomePage.blade.php
+++ resources/views/Home/HomePage.blade.php
@@ -2,10 +2,10 @@
 @section('title', 'Home')
 @section('content')
     <div class="content-wrapper">
-        <section class="spacer bg-light">
+        <section class="spacer bg-dark">
             <div class="">
                 <div class="d-flex position-relative align-items-center justify-content-center">
-                    <img alt="Hero Image" class="w-100 object-fit-cover" height="300" src="{{asset('assets/images/background/user-info.jpg')}}">
+                    <img alt="Hero Image" class="w-100 object-fit-cover" height="300" src="{{asset('assets/images/bg_hero.jpg')}}">
                     <div class="row m-auto w-100 justify-content-md-center position-absolute">
                         <div class="col-md-9 text-center">
                             <h2 class="text-white">
@@ -82,6 +82,34 @@
                                     </div>
                                 </div>
                             </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="container">
+                    <div class="d-flex gap-3 text-center pt-2 p-5">
+                        <div class="card w-100 text-center">
+                            <span class="text-2xl">Latar belakang</span>
+                            <h1 class="d-inline">E-SINTA</h1>
+                            <div class="mt-2 d-flex flex-column p-8 gap-2">
+                                <p>Kemiskinan adalah masalah serius yang dapat mempengaruhi akses keluarga terhadap layanan kesehatan, pendidikan, dan gizi yang baik. Stunting (keterlambatan pertumbuhan) pada anak-anak terkait erat dengan kondisi gizi yang buruk selama masa pertumbuhan awal. Stunting dapat memiliki dampak jangka panjang pada perkembangan fisik dan kognitif anak, serta produktivitas di masa depan. Selama ini masih belum ada data yang tersaji secara akurat dan tepat sasaran denganbaik, sehingga seringterjadi kendala atau         hambatan dalam keakuratan data terutama keluarga miskin dan stunting. Dalam pelaksanaan masih saja banyak ditemukan ketidaktepatan penerima yang seharusnya menjadi prioritas dan benar-benar membutuhkan kebutuhandasar mereka, terlebihlagi terhadap keluarga inti yang didalamnya terdapat anak stunting. Bercermin dari hal ini maka perlu adanya perubahan sistem pendataan guna menangani keluarga miskin dan stunting.</p>
+                                <p>Berkaitan dengan hal ini, maka keakuratan data terhadap warga miskin dan atau memang benar-benar membutuhkan sangat berpengaruh agar tepat sasaran dalam memenuhi kebutuhan dasar  mereka agar bisa terpenuhi gizi dan nutrisibagi keluarga inti mereka sebagaiupaya pencegahan dini stunting.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="container">
+                    <div class="d-flex gap-3 text-center">
+                        <div class="col-3 card border border-4 border-end-0 border-start-0 border-top-0 border-primary p-3">
+                            <div></div>
+                        </div>
+                        <div class="col-3 card">
+                            assda
+                        </div>
+                        <div class="col-3 card">
+                            assda
+                        </div>
+                        <div class="col-3 card">
+                            assda
                         </div>
                     </div>
                 </div>
--- resources/views/Layout/Footer.blade.php
+++ resources/views/Layout/Footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="text-center p-4">
+<footer class="text-center p-4 bg-secondary text-white">
     All Rights Reserved by Modernize Admin. Designed and Developed by
     <a href="https://adminmart.com">adminmart.com</a>
 </footer>
